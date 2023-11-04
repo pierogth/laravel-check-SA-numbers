5ae77673e54b4b47a4f34378493f07ae75edf7c2
pierogth <pierpaoloardito@gmail.com>
2023-11-04 10:26:51 +0100

    created controller and model for checks numbers

--- new-project/app/Http/Controllers/MyController.php
+++ new-project/app/Http/Controllers/MyController.php
@@ -1,26 +1,44 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
-use App\Models\User;
+use Storage;
+//require('../Models/mobileSANumber.php');
+
+use App\Models\mobileSANumber;
 use Illuminate\View\View;
- 
+use Illuminate\Http\Response; 
 class MyController extends Controller
 {
     
     public function check_number(): View
     {
-        dd("number");
+
+        $mobileNumber = new mobileSANumber();
+        echo $mobileNumber->check_number($_POST["number"]);
+        //dd("number");
         //return view('user.profile', [
         //    'user' => User::findOrFail($id)
         //]);
     }
 
-    public function check_csv(): View
+    public function check_csv()
     {
-        dd("csv");
-        //return view('user.profile', [
-        //    'user' => User::findOrFail($id)
-        //]);
+        ob_start();
+
+
+        var_dump($_FILES);
+        $tmpName = $_FILES['csv']['tmp_name'];
+
+        $outputArray=[];
+
+        $file  = fopen($tmpName,"r");
+
+        $mobileNumber = new mobileSANumber();
+        echo $mobileNumber->check_csv($file);
+
+        $file = Storage::disk('public')->get('data.csv');
+        echo asset('public/data.csv');
+        return redirect(asset('data.csv'));
+        
     }
 }
