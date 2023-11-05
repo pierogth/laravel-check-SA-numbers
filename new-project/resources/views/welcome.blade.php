0c68bbe50f453efc75b66ef938a60d72ac7421fe
pierogth <pierpaoloardito@gmail.com>
2023-11-05 08:46:29 +0100

    graphical fixes

--- new-project/resources/views/welcome.blade.php
+++ new-project/resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <body>
         <div class="container">
+            <div class="text-center p-3 mt-2"><h1>South African Number Checker</h1></div>
             <form action="/check_number" method="post">
                 @csrf
                 <!-- {{ csrf_field() }} -->
